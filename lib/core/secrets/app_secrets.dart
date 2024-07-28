cfc89182460260e16dd4858075fc1d967d57dcff
AtharvaChavan04 <114731739+AtharvaChavan04@users.noreply.github.com>
2024-07-28 17:36:27 +0530

    Update app_secrets.dart

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://YOUR_SPUABASE_URL';
+  static const supabaseUrl = 'https://YOUR_SUPABASE_URL';
   static const supabaseAnonKey =
       'ENTER YOUR SUPABASE ANON KEY';
 }
